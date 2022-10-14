67d81482d27eea5f8a55ec36eea89fa7dcb39251
Krunal Borhade <krunalborhade2897@gmail.com>
2022-10-14 23:35:33 +0530

    Registration and Login Done

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,11 @@
 const express = require("express")
+const {register,login} = require("./controllers/user.controller")
 
 const app = express()
 app.use(express.json())
+
+app.use("/register",register)
+app.use("/login",login)
 
 
 module.exports = app
---
+++ Server/src/middleware/authenticate.js
@@ -0,0 +1,28 @@
+require("dotenv").config()
+const jwt = require("jsonwebtoken")
+
+const verifyToken = (token) => {
+    return new Promise((resolve,reject) => {
+        jwt.verify(token, process.env.SECRET_KEY,(err,decoded)=>{
+            if(err){
+                return reject(err)
+            }
+            return resolve (decoded)
+        })
+    })
+}
+
+const authenticate = async(req,res,next) => {
+    let decoded;
+    try {
+        decoded = await verifyToken(token)
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.status(400).send({message:"Authorization token not found or incorrect"})
+    }
+
+    
+}
+
+module.exports = authenticate
